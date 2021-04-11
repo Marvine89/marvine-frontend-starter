e5ede6294a64661c0aca9ec65cab6adca37064ad
Marvine Bamba <mbamba@jhb.dvt.co.za>
2021-04-11 22:34:51 +0200

    add: debounce time on SearchInputComponent

--- src/app/shared/components/search-input/search-input.component.ts
+++ src/app/shared/components/search-input/search-input.component.ts
@@ -1,22 +1,47 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.scss']
 })
-export class SearchInputComponent extends FormControl {
+export class SearchInputComponent implements OnInit, OnDestroy {
   @Input() placeHolder: string = "Enter search text";
   @Input("value") value: string = "";
   @Output("onChange") inputChanged = new EventEmitter<string>();
+  ngUnsubscribe: Subscription = new Subscription();
+  valueChanged$: Subject<string> = new Subject();
 
-  constructor() {
-    super();
+  constructor() { }
+
+  ngOnInit(): void {
+    this.ngUnsubscribe.add(
+      this.valueChanged$.asObservable()
+        .pipe(debounceTime(300))
+        .subscribe((value) => {
+          this.inputChanged.emit(value);
+          console.log(value);
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.unsubscribe();
   }
 
   valueChanged(target?: any): void {
     const value = <HTMLTextAreaElement>target;
-    this.inputChanged.emit(value.value);
+    this.emitValue(value.value)
+  }
+
+  emitValue(value: string = ""): void {
+    this.valueChanged$.next(value);
+  }
+
+  clearSearch(): void {
+    this.value = "";
+    this.emitValue();
   }
 }
